b453d126ae6552a8e6fd009206fdaabea5d4e706
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-03-24 23:40:20 +0000

    some changes

--- fox_sgf_fixer.py
+++ fox_sgf_fixer.py
@@ -1,21 +1,31 @@
 import os, re
 import gofish
 
-IN_DIR = "foo"
+IN_DIR = "61on"
 OUT_DIR = "bar"
 
-all_things = list(map(lambda x : os.path.join(IN_DIR, x), os.listdir(IN_DIR)))
-all_files = list(filter(lambda x : os.path.isfile(x), all_things))
+def list_map(arr, func):
+	return list(map(func, arr))
+
+def list_filter(arr, func):
+	return list(filter(func, arr))
+
+all_things = list_map(os.listdir(IN_DIR), lambda x : os.path.join(IN_DIR, x))
+all_files = list_filter(all_things, lambda x : os.path.isfile(x))
 
 for filename in all_files:
 
     root = gofish.load(filename)
-    
+
     root.safe_commit("CA", "utf-8")
     root.safe_commit("KM", 6.5)
-    
+
     for key in ["GN", "HA", "TT", "TM", "TC", "AP"]:
         root.safe_commit(key, "")
+
+    for key in ["PW", "PB"]:
+    	if root.properties[key] == ["绝艺"]:
+    		root.properties[key] = ["绝艺 (Fine Art)"]
 
     black, white, date, gid = re.search(r"\[(.+)\]vs\[(.+)\](\d\d\d\d\d\d\d\d)(\d\d\d\d\d\d\d\d).sgf", filename).group(1, 2, 3, 4)
     newfilename = os.path.join(OUT_DIR, "{} {} vs {} ({}).sgf".format(date, black, white, gid))
