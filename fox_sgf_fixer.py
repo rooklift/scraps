2e61a13c0f25f67c5962e766723c7c2e9655d322
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-03-26 18:27:54 +0100

    add to list

--- fox_sgf_fixer.py
+++ fox_sgf_fixer.py
@@ -1,8 +1,23 @@
 import os, re
 import gofish
 
-IN_DIR = "61on"
+IN_DIR = "foo"
 OUT_DIR = "bar"
+
+KNOWN_PLAYERS = {
+	"绝艺":			"Fine Art",
+	"星宿老仙":		"Gu Li",
+	"潜伏":			"Ke Jie",
+	"剑过无声":		"Lian Xiao",
+	"airforce9":	"Kim Jiseok",
+	"Eason":		"Zhou Ruiyang",
+	"INDIANA13":	"Gu Zihao",
+	"jpgo01":		"Iyama Yuta",
+	"leaf":			"Shi Yue",
+	"maker":		"Park Junghwan",
+	"Master":		"AlphaGo",
+	"nparadigm":	"Shin Jinseo",
+}
 
 def list_map(arr, func):
 	return list(map(func, arr))
@@ -24,8 +39,9 @@ for filename in all_files:
         root.safe_commit(key, "")
 
     for key in ["PW", "PB"]:
-    	if root.properties[key] == ["绝艺"]:
-    		root.properties[key] = ["绝艺 (Fine Art)"]
+    	orig = root.properties[key][0]
+    	if orig in KNOWN_PLAYERS:
+    		root.properties[key][0] = "{} ({})".format(orig, KNOWN_PLAYERS[orig])
 
     black, white, date, gid = re.search(r"\[(.+)\]vs\[(.+)\](\d\d\d\d\d\d\d\d)(\d\d\d\d\d\d\d\d).sgf", filename).group(1, 2, 3, 4)
     newfilename = os.path.join(OUT_DIR, "{} {} vs {} ({}).sgf".format(date, black, white, gid))
