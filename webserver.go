c933edac9ed8a7eee001fc4123ff6aac151e6623
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-06-15 15:20:52 +0100

    improvements

--- webserver.go
+++ webserver.go
@@ -14,7 +14,7 @@ const (
 	FILES_DIR = "./files"
 )
 
-var known_codes = map[int]string{200: "OK", 403: "Forbidden", 404: "Not Found"}
+var known_codes = map[int]string{200: "OK", 403: "Forbidden", 404: "Not Found", 500: "Server Error"}
 
 func main() {
 	server_and_port := fmt.Sprintf("%s:%d", SERVER, PORT)
@@ -30,29 +30,48 @@ func send_error(writer http.ResponseWriter, code int) {
 func main_handler(writer http.ResponseWriter, request * http.Request) {
 
 	req_path_clean := strings.Trim(request.URL.Path, "\n\r\t /")
-	req_path_list := strings.Split(req_path_clean, "/")
 
-	local_path_list := append([]string{FILES_DIR}, req_path_list...)
-	local_filename := filepath.Join(local_path_list...)
-
-	if strings.Contains(local_filename, "..") {		// Important safety check. But there is still danger
+	if strings.Contains(req_path_clean, "..") {		// Safety check. But there is still danger
 		send_error(writer, 403)						// if the files contain a softlink to outside...
 		return
 	}
 
+	req_path_list := strings.Split(req_path_clean, "/")
+	local_path_list := append([]string{FILES_DIR}, req_path_list...)
+	local_filename := filepath.Join(local_path_list...)
+
+	// Check whether this is a directory...
+
+	stat, err := os.Stat(local_filename)
+	if err != nil {
+		send_error(writer, 500)
+		return
+	}
+
+	// If it is, try using index.html instead...
+
+	if stat.IsDir() {
+		local_path_list = append(local_path_list, "index.html")
+		local_filename = filepath.Join(local_path_list...)
+	}
+
+	// Open if possible, otherwise send error...
+
 	infile, err := os.Open(local_filename)
+	defer infile.Close()							// This is safe even if infile is nil
 
 	if err != nil {
 		err_string := fmt.Sprintf("%v", err)
 		if strings.Contains(err_string, "cannot find the file") {
 			send_error(writer, 404)
+			return
 		} else {
 			send_error(writer, 403)
+			return
 		}
-		return
 	}
 
-	defer infile.Close()
+	// Read and send the file...
 
 	buffer := make([]byte, 1024)
 
@@ -64,10 +83,11 @@ func main_handler(writer http.ResponseWriter, request * http.Request) {
 			writer.Write(buffer[0:chars])
 		}
 
-		// Note that the first call to writer.Write triggers a header write, including auto-detecting Content-Type.
+		// Note that the first call to writer.Write() triggers a header write, including auto-detecting Content-Type.
 
 		if err != nil {
 			return
 		}
 	}
 }
+
