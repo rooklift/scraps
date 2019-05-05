059e4cfa9503f70d311833d40490999126a13dcb
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2019-05-05 13:46:36 +0100

    Update ungzip.py

--- ungzip.py
+++ ungzip.py
@@ -1,12 +1,17 @@
 import gzip
 import sys
 
+from pathlib import Path
+
 infilenames = sys.argv[1:]
 assert(infilenames)
 
 for infilename in infilenames:
 
-	outfilename = infilename + ".extracted"
+	if infilename[-3:] == ".gz" and len(infilename) > 3:
+		outfilename = infilename[:-3]
+	else:
+		outfilename = infilename + ".extracted"
 
 	f = gzip.open(infilename, 'rb')
 	content = f.read()
