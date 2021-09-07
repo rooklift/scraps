812e5227878ebeda33861c39cd4ef8a2d753cfdd
rooklift <16438795+rooklift@users.noreply.github.com>
2021-09-07 23:07:31 +0100

    Update concat.py

--- concat.py
+++ concat.py
@@ -4,11 +4,13 @@ OUTFILENAME = "output.txt"
 
 files = [f for f in os.listdir(".") if os.path.isfile(f) and f != os.path.basename(__file__) and f != OUTFILENAME]
 
-strings = []
+bytearrays = []
 
 for f in files:
-	with open(f) as infile:
-		strings.append(infile.read())
+	with open(f, "rb") as infile:
+		bytearrays.append(infile.read())
 
-with open("output.txt", "w") as outfile:
-	outfile.write("\n\n".join(strings))
+with open("output.txt", "wb") as outfile:
+	for ba in bytearrays:
+		outfile.write(ba)
+		outfile.write(b"\n\n")
