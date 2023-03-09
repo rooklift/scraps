b28a8d0d4d5bdb63fa515e81431b783e0d15e97e
rooklift <16438795+rooklift@users.noreply.github.com>
2023-03-09 11:26:08 +0000

    Update entry_list_sorted.py

--- entry_list_sorted.py
+++ entry_list_sorted.py
@@ -9,25 +9,38 @@ class Player:
 
 	def __init__(self, num, name, rank, club, country):
 		self.num = int(num)
-		self.forename = name.split(",")[1].strip()
-		self.surname = name.split(",")[0].strip()
+		try:
+			self.forename = name.split(",")[1].strip()
+			self.surname = name.split(",")[0].strip()
+		except:
+			self.forename = name
+			self.surname = name
 		self.rank = rank
 		self.club = club
 		self.country = country
 
+	def is_kyu(self):
+		return self.rank.endswith("k")
+
 	def is_dan(self):
 		return self.rank.endswith("d")
+
+	def is_rankless(self):
+		return not self.is_kyu() and not self.is_dan()
 
 	def fullname(self):
 		return self.forename + " " + self.surname
 
 	def dist_to_10d(self):
-		numpart = int(self.rank[:-1])
-		if self.is_dan():
-			return 10 - numpart
+		if self.is_rankless():
+			return 100
 		else:
-			return 9 + numpart
-
+			numpart = int(self.rank[:-1])
+			if self.is_dan():
+				return 10 - numpart
+			else:
+				return 9 + numpart
+			
 	def __str__(self):
 		return "\t{0: >4}  {1: <22} {2}".format(self.rank, self.fullname(), self.club)
 
@@ -45,3 +58,4 @@ for item in sorted(players):
 	print(item)
 print()
 input()
+
