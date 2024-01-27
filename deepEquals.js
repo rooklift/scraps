686d77bea684defff4e2a34eecd8516228031d41
rooklift <16438795+rooklift@users.noreply.github.com>
2024-01-27 14:13:46 +0000

    Create deepEquals.js

---
+++ deepEquals.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// deepEquals written as an exercise - is this right?
+
+function deepEquals(a, b) {
+	if (a === b) {
+		return true;
+	}
+	if (Number.isNaN(a) && Number.isNaN(b)) {
+		return true;							// I guess?
+	}
+	if (typeof a !== "object" || typeof b !== "object") {
+		return false;
+	}
+	// So both are objects...
+	if (a === null || b === null) {
+		return false;							// We know they're not both null from the test at top
+	}
+	let a_keys = Object.keys(a);
+	let b_keys = Object.keys(b);
+	if (a_keys.length !== b_keys.length) {
+		return false;
+	}
+	for (let key of a_keys) {
+		if (!Object.hasOwn(b, key)) {
+			return false;
+		}
+		if (!deepEquals(a[key], b[key])) {
+			return false;
+		}
+	}
+	return true;
+}
