b7fca289b1ffd3ebff908a6f3b18c48695964b0b
rooklift <16438795+rooklift@users.noreply.github.com>
2025-02-16 18:20:02 +0000

    Well this sure was broken.

--- fetcher.js
+++ fetcher.js
@@ -1,42 +1,39 @@
 "use strict";
 
-// Written by Claude, mostly.
-// A caching system for if multiple components might need to fetch the same URL.
-// Note that callers might end up having references to the same promise, but this is apparently fine.
+// Partly written by Claude.
 
-let cache = new Map();			// Holds fetch response objects
-let inProgress = new Map();		// Holds fetch promises
+let cache = new Map();
+let inProgress = new Set();
 
 function fetcher(url, options = {}) {
-
-	// Must always return a promise.
 
 	if (cache.has(url)) {
 		return Promise.resolve(cache.get(url).clone());
 	}
 
-	if (inProgress.has(url)) {
-		return inProgress.get(url);
+	if (!inProgress.has(url)) {
+		inProgress.add(url);
+		fetch(url, options).then(response => {
+			cache.set(url, response);
+			inProgress.delete(url);
+		});
 	}
 
-	console.log(`Fetching: ${url}`);
-
-	let fetchPromise = fetch(url, options).then(response => {
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`);
+	let checkCache = (resolve, reject) => {
+		if (!inProgress.has(url)) {
+			if (cache.has(url)) {
+				resolve(cache.get(url).clone());
+			} else {
+				reject(new Error(`Failed to fetch ${url}`));
+			}
+		} else {
+			setTimeout(() => checkCache(resolve, reject), 100);
 		}
-		return response;
-	}).then(response => {
-		inProgress.delete(url);
-		cache.set(url, response);
-		return response.clone();
-	}).catch(error => {
-		inProgress.delete(url);
-		throw error;
-	});
+	};
 
-	inProgress.set(url, fetchPromise);
-	return fetchPromise;
+	return new Promise((resolve, reject) => {
+		setTimeout(() => checkCache(resolve, reject), 100);
+	});
 }
 
 module.exports = fetcher;
