07b06a8515358e3c640bd42e777fdad50b5421cf
rooklift <16438795+rooklift@users.noreply.github.com>
2025-02-16 19:08:41 +0000

    Update fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,45 @@
 "use strict";
 
-// Partly written by Claude.
+let cache = new Map();			// Holds fetch response objects
+let inProgress = new Map();		// Holds fetch promises
+
+// Remember multiple callers may get the same promise, which will resolve to
+// the same response unless we do something about it. What we need to do is
+// ensure each return out of this function adds a .then() which clones it.
+
+function fetcher(url, options = {}) {
+
+	// Must always return a promise.
+
+	if (cache.has(url)) {
+		return Promise.resolve(cache.get(url)).then(response => response.clone());
+	}
+
+	if (inProgress.has(url)) {
+		return inProgress.get(url).then(response => response.clone());
+	}
+
+	console.log(`Fetching: ${url}`);
+
+	let fetchPromise = fetch(url, options).then(response => {
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		cache.set(url, response);
+		return response;
+	}).finally(() => {
+		inProgress.delete(url);
+	});
+
+	inProgress.set(url, fetchPromise);
+
+	return fetchPromise.then(response => response.clone());
+}
+
+module.exports = fetcher;
+
+
+/* Alternate version, arguably simpler to understand.
 
 let cache = new Map();
 let inProgress = new Set();
@@ -36,4 +75,4 @@ function fetcher(url, options = {}) {
 	});
 }
 
-module.exports = fetcher;
+*/
