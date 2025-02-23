e68556acc4f80ac13b81ba2760fb32b9a455df04
rooklift <16438795+rooklift@users.noreply.github.com>
2025-02-23 09:29:49 +0000

    Update deepEquals.js

--- deepEquals.js
+++ deepEquals.js
@@ -7,14 +7,17 @@ function deepEquals(a, b) {
 		return true;
 	}
 	if (Number.isNaN(a) && Number.isNaN(b)) {
-		return true;							// I guess?
+		return true;								// I guess?
 	}
 	if (typeof a !== "object" || typeof b !== "object") {
 		return false;
 	}
 	// So both are objects...
 	if (a === null || b === null) {
-		return false;							// We know they're not both null from the test at top
+		return false;								// We know they're not both null from the test at top
+	}
+	if (Array.isArray(a) !== Array.isArray(b)) {
+		return false;								// Don't consider arrays to equal objects. I guess?
 	}
 	let a_keys = Object.keys(a);
 	let b_keys = Object.keys(b);
