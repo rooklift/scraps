6b49b0fffe6ebea23bb4419aa4ed1e9f1a685ac0
rooklift <16438795+rooklift@users.noreply.github.com>
2025-12-19 14:41:53 +0000

    Update kifu_snap_url_to_sgf.py

--- kifu_snap_url_to_sgf.py
+++ kifu_snap_url_to_sgf.py
@@ -4,22 +4,28 @@ data = url.split("position=")[1].split("&")[0].replace("%23", "X")		# %23 is act
 
 alpha = "abcdefghijklmnopqrstuvwxyz"
 
+BOARD_SIZE = 19
+
+for n in range(5, 26):
+	if f"board_size={n}" in url:
+		BOARD_SIZE = n
+
 AB = []
 AW = []
 
 def i_to_sgf(i):
-	x = i % 19
-	y = i // 19
+	x = i % BOARD_SIZE
+	y = i // BOARD_SIZE
 	return alpha[x] + alpha[y]
 
-for i in range(361):
+for i in range(BOARD_SIZE * BOARD_SIZE):
 	c = data[i]
 	if c == "O":
 		AW.append(i_to_sgf(i))
 	if c == "X":
 		AB.append(i_to_sgf(i))
 
-print("(;AB[", end="")
+print("(;SZ[" + str(BOARD_SIZE) + "]AB[", end="")
 print("][".join(AB), end="")
 print("]AW[", end="")
 print("][".join(AW), end="")
