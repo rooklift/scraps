c9ef9ca19420a441f077df9cecf8183bc4c7841f
rooklift <16438795+rooklift@users.noreply.github.com>
2026-02-26 13:19:35 +0000

    Update totp.py

--- totp.py
+++ totp.py
@@ -5,34 +5,51 @@ import time
 import base64
 import argparse
 
-def get_totp(secret, interval):
-	key = base64.b32decode(secret, casefold=True)
-	counter = int(time.time()) // interval
-	counter_bytes = struct.pack(">Q", counter)
-	mac = hmac.new(key, counter_bytes, hashlib.sha1).digest()
-	offset = mac[-1] & 0x0F
-	code = struct.unpack(">I", mac[offset:offset + 4])[0] & 0x7FFFFFFF
-	return str(code % 1_000_000).zfill(6)
 
 def main():
 
-	parser = argparse.ArgumentParser(description="Generate a TOTP code.")
-	parser.add_argument("secret", nargs="?", default=None, help="Base32-encoded secret key")
-	parser.add_argument("-i", "--interval", type=int, default=30, help=f"Time interval in seconds")
+	parser = argparse.ArgumentParser(description = "Generate a TOTP code.")
+	parser.add_argument("secret", nargs = "?", default = None, help = "Base32-encoded secret key")
+	parser.add_argument("-i", "--interval", type = int, default = 30, help = "Time interval in seconds")
 	args = parser.parse_args()
 
+	interval = args.interval
 	secret = args.secret
+
 	if not secret:
 		secret = input("Enter base32-encoded secret: ").strip()
 		if not secret:
 			print("No secret provided.")
 			return
 
+	loop(secret, interval)
+
+
+def loop(secret, interval):
 	while True:
-		code = get_totp(secret, args.interval)
-		seconds_left = args.interval - (int(time.time()) % args.interval)
-		print(f"{code}  (expires in {seconds_left}s)")
-		time.sleep(seconds_left + 2)
+		t = time.time()
+		code = get_totp(secret, get_counter(t, interval))
+		remaining = get_remaining_time(t, interval)
+		print(f"{code}  (expires in {int(remaining)}s)")
+		time.sleep(remaining + 0.5)
+
+
+def get_counter(unix_seconds, interval):
+	return int(unix_seconds) // interval
+
+
+def get_remaining_time(unix_seconds, interval):
+	return interval - (unix_seconds % interval)
+
+
+def get_totp(secret, counter):
+	key = base64.b32decode(secret, casefold = True)
+	counter_bytes = struct.pack(">Q", counter)
+	mac = hmac.new(key, counter_bytes, hashlib.sha1).digest()
+	offset = mac[-1] & 0x0F
+	code = struct.unpack(">I", mac[offset:offset + 4])[0] & 0x7FFFFFFF
+	return str(code % 1_000_000).zfill(6)
+
 
 if __name__ == "__main__":
 	main()
